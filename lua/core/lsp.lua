fb761a4fd263d4660784bb6b7da64d0d24b89c4c
Tamer Cuba <tamercuba@gmail.com>
2025-06-11 11:18:55 -0300

    feature: fixing lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,26 +1,25 @@
-vim.lsp.enable("lua_ls", "pyright", "gopls", "rust_analyzer", "ruff","ts_ls", "clojure_lsp" })
+vim.lsp.enable({ "lua_ls", "pyright", "gopls", "rust_analyzer", "ruff", "ts_ls", "clojure_lsp" })
 
 vim.diagnostic.config({
-    virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
+	virtual_lines = true,
+	-- virtual_text = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
-
