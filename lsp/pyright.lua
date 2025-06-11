2bcc00280f697aae4bfa1ccd198e153c2f068ae5
Tamer Cuba <tamercuba@gmail.com>
2025-06-11 11:16:24 -0300

    feature: fixing pyright typo

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,32 +1,31 @@
 local function get_python_executable()
-  local venv_path = os.getenv("VIRTUAL_ENV")
-  if venv_path then
-    return venv_path .. "/bin/python"
-  else
-    return "python" -- Default to system python if VIRTUAL_ENV is not set
-  end
+	local venv_path = os.getenv("VIRTUAL_ENV")
+	if venv_path then
+		return venv_path .. "/bin/python"
+	else
+		return "python" -- Default to system python if VIRTUAL_ENV is not set
+	end
 end
 
 return {
-cmd = { "pyright-langserver", "--stdio" },
-filetypes = { "python" },
-opts = {
-	before_init = function(params)
-	    params.processId = vim.NIL
-	    params.rootPath = vim.fn.getcwd()
-	    params.rootUri = vim.uri_from_fname(vim.fn.getcwd())
-	    params.initializationOptions = { pythonPath = get_python_executable() }
-	end
-	settings = {
-	    python = {
-		      pythonPath = get_python_executable(),
-		      typeCheckingMode = "strict",
-		      analyzeUnannotatedFunctions = false,
-		      reportReturnType = true,
-		      diagnosticMode = "openFilesOnly",
-		      useLibraryCodeForTypes = true,
-	    },
-	}
-}
-
+	cmd = { "pyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	opts = {
+		before_init = function(params)
+			params.processId = vim.NIL
+			params.rootPath = vim.fn.getcwd()
+			params.rootUri = vim.uri_from_fname(vim.fn.getcwd())
+			params.initializationOptions = { pythonPath = get_python_executable() }
+		end,
+		settings = {
+			python = {
+				pythonPath = get_python_executable(),
+				typeCheckingMode = "strict",
+				analyzeUnannotatedFunctions = false,
+				reportReturnType = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 }
