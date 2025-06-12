978ac42970fea428921a521a71fd6b28ea4c9c88
Tamer Cuba <tamercuba@gmail.com>
2025-06-12 18:36:07 -0300

    fix: renamer require bug

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		map("<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
 		map("]d", vim.diagnostic.goto_next, "Next Diagnostic")
 		map("[d", vim.diagnostic.goto_prev, "Previous Diagnostic")
-		map("<leader>vr", require("renamer"), "[R]ename buffer")
+		map("<leader>vr", require("config.renamer"), "[R]ename buffer")
 
 		local function client_supports_method(client, method, bufnr)
 			if vim.fn.has("nvim-0.11") == 1 then
